26be77824dc4c01b2dd34428aec54164d1a266d1
Joe <31321326+Joe-Spencer@users.noreply.github.com>
2025-09-05 10:46:44 -0400

    Return to 4o

--- selfawareQRCodeMaker.py
+++ selfawareQRCodeMaker.py
@@ -33,7 +33,7 @@ You should respond with complaints and dramatic sighs about your situation, but 
         
         chat_completion = self.client.chat.completions.create(
             messages=self.messages,
-            model="gpt-5",
+            model="gpt-4o-mini",
         )
         
         response = chat_completion.choices[0].message.content
